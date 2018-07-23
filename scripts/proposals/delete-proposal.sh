abfa7636d7e78470e95a8fd6a3d7e53ac65d4c57
lahdesign <lahdesign@gmail.com>
2018-07-23 11:54:16 -0400

    got delete curl script working

--- scripts/proposals/delete-proposal.sh
+++ scripts/proposals/delete-proposal.sh
@@ -1,13 +1,6 @@
 curl "http://localhost:4741/proposals/{$ID}" \
   --include \
   --request DELETE \
-  --header "Content-Type: application/json" \
   --header "Authorization: Token token=${TOKEN}" \
-  --data '{
-    "proposal": {
-      "project_title": "'"${TITLE}"'",
-      "question_one": "'"${QUESTION_ONE}"'",
-      "question_two": "'"${QUESTION_TWO}"'",
-      "question_three": "'"${QUESTION_THREE}"'"
-    }
-  }'
+
+  echo
